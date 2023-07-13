a0ab1b1ca9dd96c70b5467ed577fbf1177176434
wonjin-dev <fsdjin159@gmail.com>
2023-07-13 22:51:38 +0900

    step4 _ make up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import {useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 import BaseInput from './components/BaseInput';
+import PasswordInput from './components/PasswordInput';
+import {signInFormValidation} from './constants/validation';
 
 interface LoginForm {
 	email: string;
 	password: string;
 }
-
-const loginFormValidation = yup.object({
-	email: yup
-		.string()
-		.required('필수 입력 사항입니다.')
-		.matches(
-			/^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
-			'이메일 형식이 아닙니다'
-		),
-	password: yup
-		.string()
-		.required('필수 입력 사항입니다.')
-		.min(6, '최소 6자 이상 입력해야 합니다'),
-});
 
 const App = () => {
 	const {
@@ -29,7 +16,7 @@ const App = () => {
 		formState: {errors},
 	} = useForm<LoginForm>({
 		mode: 'onBlur',
-		resolver: yupResolver(loginFormValidation),
+		resolver: yupResolver(signInFormValidation),
 	});
 
 	const email = register('email');
@@ -43,7 +30,7 @@ const App = () => {
 		<main>
 			<form method='post' onSubmit={handleSubmit(successCallback)}>
 				<BaseInput {...email} errorMsg={errors.email && errors.email.message} />
-				<BaseInput
+				<PasswordInput
 					{...password}
 					errorMsg={errors.password && errors.password.message}
 				/>
